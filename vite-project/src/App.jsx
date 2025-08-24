90876626be862b3b813185ad548f8e12a0cc3fe2
heyitsharman <harmankaur0779@gmail.com>
2025-08-24 22:49:38 +0530

    navbar added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Contact from './pages/Contact'
+import Cart from './pages/Cart'
+import Orders from './pages/Orders'
+import Product from './pages/Product'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
-    <div>
-      
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>} />
+        <Route path='/collection' element={<Collection></Collection>}></Route>
+        <Route path='/about' element={<About></About>}/>
+        <Route path='/contact' element={<Contact></Contact>} ></Route>
+        <Route path='/product/:productId' element={<Product></Product>}></Route>
+        <Route path='/cart' element={<Cart></Cart>} />
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/place-order' element={<PlaceOrder></PlaceOrder>}></Route>
+        <Route path='/orders' element={<Orders></Orders>}></Route>
+      </Routes>
     </div>
   )
 }
