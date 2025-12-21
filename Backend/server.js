c52b249703d7b390346fa3ff6800c5ebeefe8c29
heyitsharman <harmankaur0779@gmail.com>
2025-12-21 10:08:57 +0530

    Add Backend and admin folders

---
+++ Backend/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
+import CartRouter from './routes/CartRoute.js';
+import orderRouter from './routes/orderRoute.js';
+
+
+//App config
+const app = express();
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+app.use('/api/user',userRouter);
+app.use('/api/product',productRouter);
+app.use('/api/cart',CartRouter);
+app.use('/api/order',orderRouter);
+
+
+app.get('/',(req,res)=>{
+  res.send('api working');
+})
+
+
+
+
+
+app.listen(port,()=>{
+    console.log('server started on PORT : '+ port);
+})
